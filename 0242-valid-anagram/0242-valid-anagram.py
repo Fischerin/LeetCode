1806d6838cc3ac930acf0618d699281eea0f63ff
KANGYUBIN <136579600+yonareim@users.noreply.github.com>
2023-06-22 18:19:35 +0900

    Time: 54 ms (89.99%), Space: 16.8 MB (87.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+    
+        
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            #해쉬맵 #get으로 해쉬맵이 없으면 0으로 돌아옴
+            countS[t[i]] = 1 + countS.get(s[i], 0) #t로 변경
+            
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+            
+        return True
+    
+    
+    # sort? 
+                
+            
