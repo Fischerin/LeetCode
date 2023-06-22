8971bc2fc92902591df5b3ab962278f4ca6af8bf
KANGYUBIN <136579600+yonareim@users.noreply.github.com>
2023-06-22 18:32:12 +0900

    Time: 64 ms (67.89%), Space: 17.3 MB (22.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,25 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+#         return sorted(s) == sorted(t) #sort 사용!
     
+#         if len(s) != len(t):
+#             return False
         
-        if len(s) != len(t):
-            return False
+#         countS, countT = {}, {}
         
-        countS, countT = {}, {}
-        
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            #해쉬맵 #get으로 해쉬맵이 없으면 0으로 돌아옴
-            countS[t[i]] = 1 + countS.get(s[i], 0) #t로 변경
+#         for i in range(len(s)):
+#             countS[s[i]] = 1 + countS.get(s[i], 0)
+#             #해쉬맵 #get으로 해쉬맵이 없으면 0으로 돌아옴
+#             countS[t[i]] = 1 + countS.get(s[i], 0) #t로 변경
             
-        for c in countS:
-            if countS[c] != countT.get(c,0):
-                return False
+#         for c in countS:
+#             if countS[c] != countT.get(c,0):
+#                 return False
             
-        return True
-    
-    
-    # sort? 
+#         return True
                 
             
+            return sorted(s) == sorted(t)
